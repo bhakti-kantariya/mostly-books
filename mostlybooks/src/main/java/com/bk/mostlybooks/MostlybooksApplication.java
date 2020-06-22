601d59fb3ac8c335416ef62b75a903eab7278988
128bhakti <bhakti.kantariya@somaiya.edu>
2020-06-22 14:47:03 +0530

    Initial commit. Created spring boot application and configured the application properties

---
+++ mostlybooks/src/main/java/com/bk/mostlybooks/MostlybooksApplication.java
@@ -0,0 +1,13 @@
+package com.bk.mostlybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostlybooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostlybooksApplication.class, args);
+	}
+
+}
